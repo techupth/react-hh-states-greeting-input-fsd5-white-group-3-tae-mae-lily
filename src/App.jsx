a906642ff05a55a344138098edd4c0916d8fefb8
Okubo Hazuki <okubohazuki@Okubos-MacBook-Air.local>
2023-12-24 20:48:06 +0900

    edited to save

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import "./App.css";
-import { useState } from "react";
+import { useState } from "react";                //1
+//useState hook daclare 2 state variables newMessage and greetingMessage
 
 const App = () => {
-  const [newMessage, setNewMessage] = useState("");
-  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const [newMessage, setNewMessage] = useState("");     //2
+  // newMessage store the new greeting message entred by the user.
+
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");     //3
+  // greetingMessage initialized with the default value nd representsthe current.
 
  
 
   return (
     <div className="App">
-      <div className="greeting-container">{greetingMessage}</div>
+      <div className="greeting-container">{greetingMessage}</div>     //6
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
         <input id="greeting-message" 
         type="text" 
         value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}/>
+        onChange={(e) => setNewMessage(e.target.value)}/>     //5
       </div>
 
       <div className="buttons">
-        <button onClick={() => {setGreetingMessage(newMessage)}}>Update text</button>
+        <button onClick={() => {setGreetingMessage(newMessage)}}>Update text</button>   //4
       </div>
     </div>
   );
